b79f4976c29e48b2e2aaa185b27c89a7a9e0d271
clock27-lab <68434062+clock27-lab@users.noreply.github.com>
2020-07-17 22:08:04 +0700

    PROGRAM ARDUINO SEND DAN RECEIVE

---
+++ ARDUINO_RS485_MODUL1/ARDUINO_RS485_MODUL1.ino
@@ -0,0 +1,39 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11);
+
+
+/*
+
+KONEKSI
+
+PIN ARDUINO <=============> MODUL UART TO RS485
+
+PIN 10      <=============> PIN RX
+PIN 11      <=============> PIN TX
+PIN 5V      <=============> PIN VCC
+PIN GND      <=============> PIN GND
+
+
+*/
+
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  delay (2000);
+  Serial.println("ARDUINO KONEKSI MODULE RS485");
+  delay (2000);
+  Serial.println("ARDUINO KIRIM KE RS485");
+  mySerial.println("ARDUINO 1 KIRIM KE ARDUINO 2");
+
+}
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
